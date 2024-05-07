ea4850e7a99fcb89ee2e5f8753290c47cf978a90
KimInteger <kjs20151240@gmail.com>
2024-05-07 21:26:25 +0900

    오류나서 그냥 새로 만듬

---
+++ projectFocus.js
@@ -0,0 +1,19 @@
+const root = document.getElementById('root');
+
+const leftM = document.getElementById('leftMove');
+
+const rightM = document.getElementById('rightMove');
+
+const partition = document.getElementById('partition');
+ 
+for(let i = 0; i < 3; i++) {
+  let div = document.createElement('div');
+  div.style.backgroundColor = 'white';
+  div.style.border = '3px solid black';
+  div.style.display = 'flex';
+  div.style.width = '70vw';
+  div.style.height = '60vh';
+  div.style.zIndex = 2;
+  div.style.flexShrink = 0;
+  root.appendChild(div);
+}
