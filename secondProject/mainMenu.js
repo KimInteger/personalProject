d319b949443de63df11ba3262225cde8dd28d87c
KimInteger <kjs20151240@gmail.com>
2024-05-12 16:36:46 +0900

    오늘은여기까지

---
+++ secondProject/mainMenu.js
@@ -0,0 +1,12 @@
+const root = document.getElementById('root');
+
+root.style.width = '100vw';
+root.style.height = '100vh';
+root.style.display = 'flex';
+root.style.flexDirection = 'column';
+root.style.alignItems = 'center';
+root.style.justifyContent = 'cetner';
+
+const start = document.getElementById('start');
+
+const exit = document.getElementById('exit');
