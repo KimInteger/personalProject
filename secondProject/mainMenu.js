9c6bc390560203e3b96dd5f76bfc23a1905dddfe
KimInteger <kjs20151240@gmail.com>
2024-05-26 11:29:11 +0900

    뭐야이건

--- secondProject/mainMenu.js
+++ secondProject/mainMenu.js
@@ -10,3 +10,19 @@ root.style.justifyContent = 'cetner';
 const start = document.getElementById('start');
 
 const exit = document.getElementById('exit');
+
+const test = document.getElementById('test');
+
+let windowOpen;
+
+function openWindow(){
+  windowOpen = window.open("./map/firstMap.html");
+}
+
+function closeOpenWindow(){
+  windowOpen.close();
+} 
+
+start.addEventListener('click', openWindow);
+
+exit.addEventListener('click', closeOpenWindow);
